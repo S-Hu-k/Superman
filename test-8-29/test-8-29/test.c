df6204fd4fa6f8bbeaf70cf04b75afd53e276102
Superman <2096136603@qq.com>
2019-08-29 20:54:26 +0800

    提交代码

---
+++ test-8-29/test-8-29/test.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//menu()
+//{
+//	printf("******************************************\n");
+//	printf("*****       1.add       2.sub        *****\n");
+//	printf("*****       3.mul       4.div        *****\n");
+//	printf("*****       0.exit                   *****\n");
+//	printf("******************************************\n");
+//
+//}
+//enum Option
+//{
+//	EXIT,
+//	ADD,
+//	SUB,
+//	MUL,
+//	DIV
+//};
+//int main()
+//{
+//	int input = 0;
+//	do
+//	{
+//		
+//		menu();//菜单
+//		printf("请选择:>");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case ADD:
+//			break;
+//		case SUB:
+//			break;
+//		case MUL:
+//			break;
+//		case DIV:
+//			break;
+//		case EXIT:
+//			printf("退出程序\n");
+//			break;
+//		default:
+//			printf("选择错误，请重新选择\n");
+//		}
+//	} while (input);
+//	return 0;
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
