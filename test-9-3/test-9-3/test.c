a0cc34b49a804859890038abdffc206ab93910db
Superman <2096136603@qq.com>
2019-09-03 20:22:20 +0800

    提交代码

---
+++ test-9-3/test-9-3/test.c
@@ -0,0 +1,122 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//char my_strlen(const char* str)
+//{
+//	return 0;
+//}
+//int main()
+//{
+//	//指针数组
+//	char* arr[10];
+//		//数组指针变量
+//		char* (*p)[10] = &arr;
+//		//函数指针
+//		char (*ptr)(const char*)=my_strlen;
+//		//函数指针数组
+//		char(*ptrarr[10])(const char*) = my_strlen;
+//
+//	system("pause");
+//	return 0;
+//}
+int main()
+{
+	/*sizeof(数组名)数组名单独在sizeof内部时，数组名表示的是整个数组的大小，单位是字节；
+		&数组名; 取的是整个数组的地址，单位也是字节；*/
+	
+//	int a[] = { 1, 2, 3, 4 };
+//printf("%d\n", sizeof(a));
+//printf("%d\n", sizeof(a + 0));
+//printf("%d\n", sizeof(*a));
+//printf("%d\n", sizeof(a + 1));
+//printf("%d\n", sizeof(a[1]));
+//printf("%d\n", sizeof(&a));
+//printf("%d\n", sizeof(*&a));
+//printf("%d\n", sizeof(&a + 1));
+//printf("%d\n", sizeof(&a[0]));
+//printf("%d\n", sizeof(&a[0] + 1));
+//比特科技
+////字符数组
+//char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+//printf("%d\n", sizeof(arr));
+//printf("%d\n", sizeof(arr + 0));
+//printf("%d\n", sizeof(*arr));
+//printf("%d\n", sizeof(arr[1]));
+//printf("%d\n", sizeof(&arr));
+//printf("%d\n", sizeof(&arr + 1));
+//printf("%d\n", sizeof(&arr[0] + 1));
+//printf("%d\n", strlen(arr));
+//printf("%d\n", strlen(arr + 0));
+//printf("%d\n", strlen(*arr));
+//printf("%d\n", strlen(arr[1]));
+//printf("%d\n", strlen(&arr));
+//printf("%d\n", strlen(&arr + 1));
+//printf("%d\n", strlen(&arr[0] + 1));
+//char arr[] = "abcdef";
+//printf("%d\n", sizeof(arr));
+//printf("%d\n", sizeof(arr + 0));
+//printf("%d\n", sizeof(*arr));
+//printf("%d\n", sizeof(arr[1]));
+//printf("%d\n", sizeof(&arr));
+//printf("%d\n", sizeof(&arr + 1));
+//printf("%d\n", sizeof(&arr[0] + 1));
+//printf("%d\n", strlen(arr));
+//printf("%d\n", strlen(arr + 0));
+//printf("%d\n", strlen(*arr));
+//printf("%d\n", strlen(arr[1]));
+//printf("%d\n", strlen(&arr));
+//printf("%d\n", strlen(&arr + 1));
+//printf("%d\n", strlen(&arr[0] + 1));
+//char *p = "abcdef";
+//printf("%d\n", sizeof(p));
+//printf("%d\n", sizeof(p + 1));
+//printf("%d\n", sizeof(*p));
+//printf("%d\n", sizeof(p[0]));
+//printf("%d\n", sizeof(&p));
+//printf("%d\n", sizeof(&p + 1));
+//printf("%d\n", sizeof(&p[0] + 1));
+//printf("%d\n", strlen(p));
+//printf("%d\n", strlen(p + 1));
+//printf("%d\n", strlen(*p));
+//printf("%d\n", strlen(p[0]));
+//printf("%d\n", strlen(&p));
+//printf("%d\n", strlen(&p + 1));
+//printf("%d\n", strlen(&p[0] + 1));
+//
+////二维数组
+//int a[3][4] = { 0 };
+//printf("%d\n", sizeof(a));
+//printf("%d\n", sizeof(a[0][0]));
+//printf("%d\n", sizeof(a[0]));
+//printf("%d\n", sizeof(a[0] + 1));
+//printf("%d\n", sizeof(*(a[0] + 1)));
+//printf("%d\n", sizeof(a + 1));
+//printf("%d\n", sizeof(*(a + 1)));
+//printf("%d\n", sizeof(&a[0] + 1));
+//printf("%d\n", sizeof(*(&a[0] + 1)));
+//printf("%d\n", sizeof(*a));
+//printf("%d\n", sizeof(a[3]));
+//system("pause");
+//	return 0;
+//}
+	//struct address {
+	//	char name[50];
+	//	char street[50];
+	//	int phone;
+	//};
+
+	//int main()
+	//{
+	//	printf("name offset = %d byte in address structure.");
+	//		offsetof(struct address, name);
+
+	//		printf("street offset = %d byte in address structure.");
+	//		offsetof(struct address, street);
+
+	//		printf("phone offset = %d byte in address structure.");
+	//		offsetof(struct address, phone);
+
+	//	return(0);
+	//}
+	
